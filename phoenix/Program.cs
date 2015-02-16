73c2feb9e5079b2dd27cc57951adda82f87ece54
gitJames <git@vd0.net>
2015-02-16 12:00:01 -0800

    test 1

--- phoenix/Program.cs
+++ phoenix/Program.cs
@@ -2,14 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace phoenix
 {
-    class Program
+
+    public class ActiviyDooer
     {
-        static void Main(string[] args)
+
+        public int repitition { get; set; }
+
+        public ActiviyDooer()
         {
+            repitition = 5;
+
+        }
+
+
+        public ActiviyDooer(int reps)
+        {
+            repitition = reps;
+
+        }
+        void DoIt()
+        {
+            var dog = new Poodle();
+            dog.IsDone += dog_IsDone;
+        
+        }
+
+        void dog_IsDone(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
+    
+    class Dog
+    {
+        public string dtype { get; set; }
+
+        public event EventHandler IsDone;
+
+        public void Activity(string Title)
+        {
+            if (IsDone != null)
+                IsDone(this, EventArgs.Empty);
+
+        }
+
+
+    }
+
+
+    class Poodle : Dog
+    {
+        void x()
+        {
+            dtype = "test";
+
+        }
+    }
+
+
+    class Program
+    {
+        public string testprop { get; set; }
+        static void Main(string[] args)
+        {
+            var t = new ActiviyDooer(5);
+
+            var t3 = new ActiviyDooer();
+
+            var range = Enumerable.Range(2014-100, 85).Reverse();
+
+            foreach (var item in range)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Debugger.Break();
+            
+        }
+    }
+
 }
